23551abb9a00f225b34fdbb102321bbdc3f109b5
Maciej Nowak <maciejt.nowak@gmail.com>
2023-08-16 17:12:18 +0200

    Use python.exe instead of python

--- gdb/python/python.c
+++ gdb/python/python.c
@@ -2400,7 +2400,7 @@ py_initialize ()
 #ifdef WIN32
     gdb::unique_xmalloc_ptr<char> progname
       (concat (ldirname (python_libdir.c_str ()).c_str (), SLASH_STRING, "Scripts",
-	      SLASH_STRING, "python", (char *) NULL));
+	      SLASH_STRING, "python.exe", (char *) NULL));
 #else
     gdb::unique_xmalloc_ptr<char> progname
       (concat (ldirname (python_libdir.c_str ()).c_str (), SLASH_STRING, "bin",
